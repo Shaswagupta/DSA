9a38f84534e7183743b3b6cc39e354f9884e331c
Shaswat Gupta <shaswagupta@gmail.com>
2026-08-23 00:32:38 +0530

    Time: 0 ms (100%), Space: 7.9 MB (12.59%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isPowerOfTwo(int n) {
+        
+        for(long long i = 1; i <= n; i = i * 2) {
+            
+            if(i == n) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+};
